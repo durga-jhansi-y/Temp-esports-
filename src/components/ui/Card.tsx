ecea854e8867d9aa083cf6a08851f90d7a85ae6a
keepa <keepa6421@gmail.com>
2026-07-19 12:38:09 -0400

    Complete Epic 2 shared UI components

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Card.module.css';
 
-export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface CardProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'title'> {
   title?: React.ReactNode;
   description?: React.ReactNode;
 }
