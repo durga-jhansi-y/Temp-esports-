b79058c4a176c6aea61d1397cb10016793bf06d3
Yunjong123 <heo789987@gmail.com>
2026-07-20 09:49:32 -0400

    Add MainLayout for Epic 2 layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// Components
-import NavBar from './components/navBar';
+// Layout
+import MainLayout from './layouts/MainLayout';
 
 // Pages
 import LandingPage from './pages/LandingPage';
@@ -17,22 +17,19 @@ import LeagueRegistration from './pages/LeagueRegistration';
 function App() {
   return (
     <Router>
-      <div style={{ minHeight: '100vh', backgroundColor: '#0A0A0B', display: 'flex', flexDirection: 'column' }}>
-        <NavBar />
-        <main style={{ flexGrow: 1 }}>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/trust" element={<TrustPage />} />
-            <Route path="/pricing" element={<PricingPage />} />
-            <Route path="/questions" element={<QuestionsPage />} />
-            
-            {/* Auth and Registration Routes */}
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegistrationPage />} />
-            <Route path="/launch" element={<LeagueRegistration />} />
-          </Routes>
-        </main>
-      </div>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/trust" element={<TrustPage />} />
+          <Route path="/pricing" element={<PricingPage />} />
+          <Route path="/questions" element={<QuestionsPage />} />
+
+          {/* Auth and Registration Routes */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/launch" element={<LeagueRegistration />} />
+        </Routes>
+      </MainLayout>
     </Router>
   );
 }
